8c38937b85681c6cd593171f5f288ce4b2a7b402
NM Siddharth <siddharthnm07@gmail.com>
2024-10-13 21:43:53 +0530

    Updated demo2 through eclipse

--- Practise/src/practise_folder/Demo2.java
+++ Practise/src/practise_folder/Demo2.java
@@ -5,7 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi siddu");
-		System.out.println("Hi Somu");
+		System.out.println("Hi My3");
 	}
 
 }
